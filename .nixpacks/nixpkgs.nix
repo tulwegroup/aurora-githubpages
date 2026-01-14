a52357fd8a3c249fc643ee4f6b7aac26e720bb8c
Aurora OSI Deploy <aurora-deploy@github.com>
2026-01-14 10:49:59 -0800

    Fix: Declare all required Python packages in nixpkgs.nix and force both Python+Node providers in config.toml

--- .nixpacks/nixpkgs.nix
+++ .nixpacks/nixpkgs.nix
@@ -5,7 +5,19 @@ with pkgs;
   python311
   python311Packages.pip
   python311Packages.virtualenv
+  python311Packages.setuptools
+  python311Packages.wheel
   python311Packages.gunicorn
   python311Packages.uvicorn
+  python311Packages.psutil
+  python311Packages.psycopg2
+  python311Packages.redis
+  python311Packages.celery
+  python311Packages.numpy
+  python311Packages.scipy
+  python311Packages.requests
+  python311Packages.pydantic
+  python311Packages.fastapi
+  python311Packages.python-dotenv
   nodejs_22
 ]
